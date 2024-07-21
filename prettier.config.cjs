5793c39d856bbf36f14ac13b8c9012c3f2c591f8
Domantas Mauruča <domantas.mauruca@simbuka.com>
2024-07-21 23:02:46 +0300

    Add Prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const configuration = {
+    useTabs: true,
+    tabWidth: 4,
+    plugins: ["@prettier/plugin-php"],
+};
+
+module.exports = configuration;
